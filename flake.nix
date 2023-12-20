3ab55fdab47c5eace0f5edf06d3abd5928ffbf47
avi <avi.ruiz@proton.me>
2023-12-20 00:44:52 -0600

    first commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Backend Framework based on Express";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [pkgs.bashInteractive];
+        buildInputs = with pkgs; [
+          bun
+        ];
+      };
+    });
+}
